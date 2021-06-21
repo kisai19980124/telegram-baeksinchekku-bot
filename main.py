43d862dfc0d654e8461bc7e50570a9193cad2dc8
kisai19980124 <qiji19980124@gmail.com>
2021-06-22 07:01:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN,
-                          webhook_url="https://<appname>.herokuapp.com/" + TOKEN)
+                          webhook_url="https://"+HEROKU_APP_NAME+".herokuapp.com/" + TOKEN)
     updater.start_polling()
 
 
